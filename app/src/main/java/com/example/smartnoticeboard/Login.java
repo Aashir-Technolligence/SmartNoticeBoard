e7dc11a57d654862ca2c824b45e12c3a30f65a1e
basit96 <pakkobra007@gmail.com>
2019-12-25 14:12:33 +0500

    login+signup+register front-end form

--- app/src/main/java/com/example/smartnoticeboard/Login.java
+++ app/src/main/java/com/example/smartnoticeboard/Login.java
@@ -1,14 +1,35 @@
 package com.example.smartnoticeboard;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Login extends AppCompatActivity {
+    Button btnLogin,btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnLogin=(Button) findViewById(R.id.login);
+        btnSignup=(Button) findViewById(R.id.sigup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Signup.class));
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Login Click", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/smartnoticeboard/Signup.java
+++ app/src/main/java/com/example/smartnoticeboard/Signup.java
@@ -2,13 +2,31 @@ package com.example.smartnoticeboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
+    Button btnLogin,btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        btnLogin=(Button) findViewById(R.id.login);
+        btnSignup=(Button) findViewById(R.id.sigup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup.this,Register.class));
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup.this,Login.class));
+            }
+        });
     }
 }
