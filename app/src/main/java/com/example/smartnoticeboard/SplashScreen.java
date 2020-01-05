c16bbda043b967575b72a6adaa1d93b21d7d927a
Bilaleng <nicemehar24@gmail.com>
2020-01-05 22:41:43 +0500

    splashScreen

---
+++ app/src/main/java/com/example/smartnoticeboard/SplashScreen.java
@@ -0,0 +1,50 @@
+package com.example.smartnoticeboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_splash_screen );
+        ImageView imageView = findViewById( R.id.imagelogo );
+        Animation animation = AnimationUtils.loadAnimation( getApplicationContext(),R.anim.fade );
+        imageView.startAnimation( animation );
+
+        Thread timer = new Thread(  ) {
+            @Override
+            public void run() {
+                try {
+                    sleep( 4000 );
+
+                    SharedPreferences prefs = getSharedPreferences("Log", MODE_PRIVATE);
+                    boolean isLoggedIn = prefs.getBoolean("isLoggedIn", false);
+                    if (isLoggedIn) {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class );
+                        startActivity( intent );
+                        finish();
+                    } else {
+                        Intent intent = new Intent(getApplicationContext(), Login.class );
+                        startActivity( intent );
+                        finish();
+                    }
+
+
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        timer.start();
+    }
+}
